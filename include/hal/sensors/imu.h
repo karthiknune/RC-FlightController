c8118034fd8a661d5c8fb04efe6e75543e762c05
mukund-vishwanath <mukundvishwanathacademic@gmail.com>
2026-04-15 01:38:03 -0400

    Update imu.h
    simple C‑style interface with the class of the imu data

--- include/hal/sensors/imu.h
+++ include/hal/sensors/imu.h
@@ -1,6 +1,25 @@
+//Previous code - 
+// #pragma once
+// #include "datatypes.h"
+
+// void IMU_Init();
+
+// void IMU_Read(IMUData_raw &data);
+
+//New code 
 #pragma once
-#include "datatypes.h"
 
-void IMU_Init();
+struct imu_sample_t {
+    float ax_g;
+    float ay_g;
+    float az_g;
+    float gx_dps;
+    float gy_dps;
+    float gz_dps;
+    float mx_uT;
+    float my_uT;
+    float mz_uT;
+};
 
-void IMU_Read(IMUData_raw &data);
+bool imu_init();                 // wraps imu.begin + configure + calibration
+bool imu_read_sample(imu_sample_t &sample);  // wraps imu.readSample()
