646cd83abc6e6856a359f57599d069cd8375ef08
karthiknune <nunekarthik27@gmail.com>
2026-04-15 19:17:46 -0400

    modified manual mode logic to give full pilot cotrol. added rc_data structure. changed default flight mode to manual. added get_flight_mode_pwm() function. updated commit message.

--- include/hal/comms/rx_spektrum.h
+++ include/hal/comms/rx_spektrum.h
@@ -1,15 +1,17 @@
 #pragma once
 
+extern RCData rc_data;
+
 void rx_init();
 void rx_read();  ///read rc channel raw pwms
 
-
-
 ///rc channel pwm to target roll pitch setpoints
+//all these return angles in degrees, throttle in 0-1000 range
 float get_des_roll();
 float get_des_pitch();
 float get_des_yaw();
 float get_des_throttle();
+
 float get_flight_mode_pwm();
 
 
